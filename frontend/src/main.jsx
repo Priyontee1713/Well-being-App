e911a29dc3d324cff7058124974134c38896c07c
IsnatSami <167467099+IsnatSami@users.noreply.github.com>
2026-01-07 21:23:03 +0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index_rituals.css'
 import App from './App_rituals.jsx'
 
 createRoot(document.getElementById('root')).render(
